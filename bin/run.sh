de3a835ab8d24030b2a9014943bd86e67f4a7931
John Carroll <jcarroll@macadmins-MBP-4.fios-router.home>
2020-11-02 15:02:18 -0500

    edit run.sh for testing crash

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+rand_mod=$(expr $RANDOM % 5)
+if [ $rand_mod -eq 0 ]; then
+    echo "Starting divisible by five app"
+else
+    echo "Crashing non-divisible by five app";
+    exit 1
+fi
 # Set environment options to exit immediately if a non-zero status code
 # appears from a command or within a pipe
 set -o errexit
